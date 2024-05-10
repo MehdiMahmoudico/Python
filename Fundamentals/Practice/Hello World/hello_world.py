ac8a7c63106d92cb47c4d252075dd9e3235e7d94
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-05-10 22:45:06 +0100

    hello world

--- Fundamentals/Practice/Hello World/hello_world.py
+++ Fundamentals/Practice/Hello World/hello_world.py
@@ -15,6 +15,6 @@ print( "hello", age )	# with a comma
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print( "I love to eat" , fave_food1 ,"and", fave_food2 ) # with .format()
+print( "I love to eat {} and {}.".format(fave_food1,fave_food2)) # with .format()
 print(f"i love to eat {fave_food1} and {fave_food2}") # with an f string
 
