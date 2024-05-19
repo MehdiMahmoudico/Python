dd275ae1dde05f3b44cc54d7cdd34e6e8d117d46
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-05-19 11:48:04 +0100

    basket

--- Fundamentals/Practice/Basketball Dictionaries/basket.py
+++ Fundamentals/Practice/Basketball Dictionaries/basket.py
@@ -1,11 +1,13 @@
 class Player:
-    def __init__(self, info):
-        self.name = info["name"]
-        self.age = info["age"]
-        self.position = info["position"]
-        self.team = info["team"]
+        def __init__(self, info):
+                self.name = info["name"]
+                self.age = info["age"]
+                self.position = info["position"]
+                self.team = info["team"]
 
-	
+        def __repr__(self) :
+                return f"Player : {self.name}, age : {self.age}, Position : {self.position}, Team : {self.position}"
+
 
 kevin = {
         "name": "Kevin Durant", 
@@ -25,26 +27,49 @@ kyrie = {
         "team": "Brooklyn Nets"
 }
 
-playerData=[kevin,jason,kyrie]
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33, "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32, "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+    	"name": "", 
+    	"age":16, 
+    	"position": "P", 
+    	"team": "en"
+    }
+]
+
 
 newTeam=[]
 
-for data in playerData:
+for data in players:
     player = Player(data)
     newTeam.append(player)
 
 
 for player in newTeam:
-    print(player)
-    
-
-	
-# player_jason = Player(jason)
-# player_kevin = Player(kevin)
-# player_kyrie = Player(kyrie)
-
-
-# print(player_jason)
-# print(player_kevin)
-# print(player_kyrie)
-
+    print(player)  
