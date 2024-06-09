67a6144ad1e429461f0649e8b3a8bdb44a6f1ee3
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-06-09 13:21:00 +0100

    try

---
+++ Flask_mysql/Practice/Users CRUD/flask_app/controllers/users.py
@@ -0,0 +1,25 @@
+from flask_app import app
+
+from flask import render_template , redirect , request
+
+from flask_app.models.user import User
+
+@app.route("/")
+def read():
+    user = User.get_all()
+    return render_template("readall.html",users=user)
+
+@app.route("/users", methods=["POST"])
+def create():
+    User.create(request.form)
+    return redirect ("/")
+
+@app.route("/newuser",methods=["POST"] )
+def createNew():
+    return render_template ("create.html")
+
+@app.route("/users/<int:id>/delete")
+def delete(id):
+    data = {"id":id}
+    User.delete(data)
+    return redirect ("/")
